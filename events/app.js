33519997c2162be5ed61e69e833c62572bb37d30
ronak <rk8875636542@gmail.com>
2023-03-22 16:03:18 +0530

    update everything

---
+++ events/app.js
@@ -0,0 +1,53 @@
+// ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
+// ********** fixed navbar ************
+
+function changePannel(evt, pannelName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("pannel");
+  console.log(x.length);
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("event-cat-but");
+  console.log(tablinks.length);
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" whiteBG", " ");
+  }
+  document.getElementById(pannelName).style.display = "grid";
+  url = window.location.href.split("?")[0] + ("?" + pannelName);
+  window.history.replaceState({}, "", url);
+  evt.currentTarget.className += " whiteBG";
+}
+
+$(document).ready(function () {
+  var path = window.location.href;
+  path = path.split("?").pop();
+  if (
+    path == "coding" ||
+    path == "mechanical" ||
+    path == "gaming" ||
+    path == "entc" ||
+    path == "robotics" ||
+    path == "management" ||
+    path == "open-events" ||
+    path == "isdf"
+  ) {
+    document.getElementById(path).style.display = "grid";
+    document.getElementById("infoscreen").style.display = "none";
+    document.getElementById(path + "btn").className += " whiteBG";
+  }
+});
---
+++ google-forms/script.js
@@ -0,0 +1,85 @@
+var events_link = [
+    ["Code Red", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"], // 0
+    ["Bug off", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 1
+    ["Reverse coding", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 2
+    ["only girls coding event", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 3
+  
+    ["NAND IT", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 4
+    ["IMPEDENCE", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 5
+  
+    ["Catia", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true" ],// 6
+    ["AutoCAD", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 7
+  
+    ["CODM (BR & MP)", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 8
+    ["Valorant", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 9
+    ["clash royal", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 10
+    ["Free fire", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 11
+  
+    ["Tech matrix", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 12
+    ["Sudoku tech", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 13
+    ["placement APTI", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 14
+    ["Rubix Cube", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 15
+    ["photoshop event for poster", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 16
+    ["Ad making", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 17
+    ["photography", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 18
+  
+    ["SCINTILLA-IGNITING MINDS", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 19
+    ["ABHIKALPNA :BUDDY LOVE THE WAY YOU DESIGN", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 20
+    ["QUIZOTICS", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 21
+  
+    ["Innovation Challenge: A Day in a Startup", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 22
+    ["Startup Challenge: A Day in a Startup", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 23
+  
+    ["ByPass CTF", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 24
+  
+    ["TROUBLESHOOTING THE ENCLOSURE"," https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true" ],// 25
+    ["TECHSTORM", " https://docs.google.com/forms/d/e/1FAIpQLSdmmWs0qrQ1Y6HHGB_MW7Q0WXoqGwlrDUrOphNbgL_UgFdmWg/viewform?embedded=true"],// 26
+  ];
+
+console.log("lodod");
+// ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
+// ****************** form height ***************
+
+    
+
+
+    window.addEventListener("load", (event) => {
+        console.log("page is fully loaded");
+        
+        const waiting_loader=document.querySelector(".waiting")
+
+        const forms=document.querySelector(".forms")
+       
+        
+        // var URL = new URL(window.location.href);
+        // const id = URL.searchParams.get('eventId');
+        // const name=URL.searchParams.get('eventName');
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const id =urlParams.get('eventId');
+
+        // waiting_loader.style.display='none';
+        forms.innerHTML=`<iframe class="form" src="${events_link[id][1]}" width="640" height="6259" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`;
+// height logic
+        const iframe =document.querySelector("iframe");
+        console.log(iframe.height)
+        let height=iframe.height;
+        console.log(height)
+        height = Number(height)
+        forms.style.height=`${height}px`;
+        console.log('done')
+    });
